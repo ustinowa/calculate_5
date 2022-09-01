642f432b8753ce4543b06d1bc3d16962640186e6
ZMichael3012 <michael.zaplatin@yandex.ru>
2022-09-02 01:31:16 +0700

    Added logger and complex calculator

--- logger.py
+++ logger.py
@@ -1,2 +1,21 @@
-def func():
-    pass
+from datetime import datetime
+
+
+def report_sum(a, b, summary, mode):
+    with open('log.txt', 'a+') as file:
+        file.write(f'--{datetime.now()}--   {mode} numbers: {a} + {b} = {summary}')
+
+
+def report_multiply(a, b, multiplication, mode):
+    with open('log.txt', 'a+') as file:
+        file.write(f'--{datetime.now()}--   {mode} numbers: {a} * {b} = {multiplication}')
+
+
+def report_sub(a, b, subtraction, mode):
+    with open('log.txt', 'a+') as file:
+        file.write(f'--{datetime.now()}--   {mode} numbers: {a} - {b} = {subtraction}')
+
+
+def report_div(a, b, division, mode):
+    with open('log.txt', 'a+') as file:
+        file.write(f'--{datetime.now()}--   {mode} numbers: {a} / {b} = {division}')
